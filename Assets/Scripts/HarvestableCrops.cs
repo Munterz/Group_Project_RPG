429297d5ea595871bf1cd5c508cefa90c209b1bf
Hunzlah <56739241+Hunzlah@users.noreply.github.com>
2025-01-08 15:11:38 +0500

    Bug fix

--- Assets/Scripts/HarvestableCrops.cs
+++ Assets/Scripts/HarvestableCrops.cs
@@ -7,7 +7,7 @@ public class HarvestableCrops : MonoBehaviour
     private void Awake ()
     {
         //PlayerPrefs.SetInt("keyValue", 1);//temp
-        if (PlayerPrefs.GetInt("keyValue") != 1)
+        if (PlayerPrefs.GetInt("keyValue") > 1)
         {
             gameObject.SetActive(false);
         }
