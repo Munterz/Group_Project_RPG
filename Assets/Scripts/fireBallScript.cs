5c42c998bb1d9181dfd60823bbea70f2e25c3258
Munterz <munterskevin@hotmail.com>
2025-01-08 02:37:50 +0100

    Fireball do dmg now

--- Assets/Scripts/fireBallScript.cs
+++ Assets/Scripts/fireBallScript.cs
@@ -1,26 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class fireBallScript : MonoBehaviour {
     private Rigidbody2D rb;
-    private GameObject[] target;
     public float moveSpeed = 3;
     private Vector2 moveDirection;
-    // Start is called before the first frame update
+
     void Start() {
         rb = GetComponent<Rigidbody2D>();
-        target = GameObject.FindGameObjectsWithTag("Player");
-        moveDirection = (target[0].transform.position - transform.position).normalized * moveSpeed;
+        GameObject target = GameObject.FindGameObjectWithTag("Player");
+        if (target != null) {
+            moveDirection = (target.transform.position - transform.position).normalized * moveSpeed;
+            rb.velocity = new Vector2(moveDirection.x, moveDirection.y);
+        }
     }
 
-    // Update is called once per frame
-    void Update() {
-        rb.velocity = new Vector2(moveDirection.x, moveDirection.y);
-    }
-
-    public void OnTriggerEnter2D(Collider2D collision) {
-        if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Wall") || collision.gameObject.CompareTag("Enemy")) {
+    void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.CompareTag("Player") ||
+            collision.gameObject.CompareTag("Wall") ||
+            collision.gameObject.CompareTag("Enemy")) {
             Destroy(gameObject);
         }
     }
